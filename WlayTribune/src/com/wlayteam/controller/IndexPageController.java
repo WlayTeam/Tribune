d5d06e5207914658ce02f1ce33598948573497f2
LxAshin <lx_ashin@163.com>
2016-11-08 21:48:50 +0800

    forward indexpage

--- WlayTribune/src/com/wlayteam/controller/IndexPageController.java
+++ WlayTribune/src/com/wlayteam/controller/IndexPageController.java
@@ -1,3 +1,9 @@
+/**
+ * @description the index request with spring MVC
+ * @function display the index page and display the latest article 
+ * @time 11/8
+ * @author ashin
+ */
 package com.wlayteam.controller;
 
 import org.springframework.stereotype.Controller;
@@ -6,7 +12,11 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexPageController {
-
+	/**
+	 * display index page
+	 * @param modelAndView
+	 * @return
+	 */
 	@RequestMapping(value = "toIndex")
 	public ModelAndView toIndex(ModelAndView modelAndView) {
 		modelAndView.setViewName("index");
