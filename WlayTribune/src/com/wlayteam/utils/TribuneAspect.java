c4321cefd56d49ee917101be0cbe59a2aa636c14
LxAshin <lx_ashin@163.com>
2016-11-13 22:53:10 +0800

    aspect final alert

--- WlayTribune/src/com/wlayteam/utils/TribuneAspect.java
+++ WlayTribune/src/com/wlayteam/utils/TribuneAspect.java
@@ -1,3 +1,9 @@
+/**
+ * @description the aspect class 
+ * @function  add log to record the method weather be executed and execution completed
+ * @time 11/13
+ * @author ashin
+ */
 package com.wlayteam.utils;
 
 import org.aspectj.lang.ProceedingJoinPoint;
@@ -6,13 +12,18 @@ import org.aspectj.lang.annotation.Aspect;
 
 @Aspect
 public class TribuneAspect {
-	
-	@Around(value="execution (* com.wlayteam..*.*(..))")
-	public Object aroundMethodExecute(ProceedingJoinPoint proceedingJoinPoint) throws Throwable{
-		System.out.println("the method will be executed: "+proceedingJoinPoint);
+	/**
+	 * proxy  all the method under the 'com.wlayteam' package
+	 * @param proceedingJoinPoint
+	 * @return 
+	 * @throws Throwable
+	 */
+	@Around(value = "execution (* com.wlayteam..*.*(..))")
+	public Object aroundMethodExecute(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+		System.out.println("the method will be executed: " + proceedingJoinPoint);
 		Object object = proceedingJoinPoint.proceed();
-		System.out.println("the method has executed: "+proceedingJoinPoint);
+		System.out.println("the method has executed: " + proceedingJoinPoint);
 		return object;
 	}
-	
+
 }
