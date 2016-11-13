b96084191932cfec7430c8e7bcc47ea1f95f8528
LxAshin <lx_ashin@163.com>
2016-11-13 21:44:05 +0800

    add aspects to advice method wheather begain or end

--- WlayTribune/src/com/wlayteam/utils/TribuneAspect.java
+++ WlayTribune/src/com/wlayteam/utils/TribuneAspect.java
@@ -1,13 +1,20 @@
 package com.wlayteam.utils;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 
 @Aspect
 public class TribuneAspect {
 	
-	@Before(value="exectuion(* com.wlayteam.controller.IndexPageController.toIndex(..)")
-	public void beforefMthodExecute(){
-		System.out.println("method executed");
+	@Before(value="execution (* com.wlayteam.controller..*.*(..))")
+	public void beforefMthodExecute(JoinPoint joinPoint){
+		System.out.println("the method will be executed: "+joinPoint);
+	}
+	
+	@After(value="execution (* com.wlayteam.controller..*.*(..))")
+	public void afterMethodExecute(JoinPoint joinPoint){
+		System.out.println("the method has executed: "+joinPoint);
 	}
 }
