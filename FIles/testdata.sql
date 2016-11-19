c8ec51030f38475d5f2f54964dadd841586a0cf2
LxAshin <lx_ashin@163.com>
2016-11-20 03:13:28 +0800

    add test sql

---
+++ FIles/testdata.sql
@@ -0,0 +1,15 @@
+insert into title values(1,"深入解析OutOfMemoryError","在Java中，所有对象都存储在堆中。他们通过new关键字来进行分配，JVM会检查是否所有线程都无法在访问他们了，并且会将他们进行回收。在大多数时候程序员都不会有一丝一毫的察觉，这些工作都被静悄悄的执行。但是，有时候在发布前的最后一天，程序挂了。");
+
+insert into title values(2,"为什么1000 == 1000返回为False，而100 == 100会返回为True?","我们知道，如果两个引用指向同一个对象，那么==就成立；反之，如果两个引用指向的不是同一个对象，那么==就不成立，即便两个引用的内容是一样的。因此，结果就会出现false。");
+
+insert into title values(3,"深入解析OutOfMemoryError","在Java中，所有对象都存储在堆中。他们通过new关键字来进行分配，JVM会检查是否所有线程都无法在访问他们了，并且会将他们进行回收。在大多数时候程序员都不会有一丝一毫的察觉，这些工作都被静悄悄的执行。但是，有时候在发布前的最后一天，程序挂了。");
+
+
+
+insert into title values(4,"为什么1000 == 1000返回为False，而100 == 100会返回为True?","我们知道，如果两个引用指向同一个对象，那么==就成立；反之，如果两个引用指向的不是同一个对象，那么==就不成立，即便两个引用的内容是一样的。因此，结果就会出现false。");
+
+insert into summary values (1,"","shenzhang ","20161120",1);
+insert into summary values (2,"","shenzhang ","20161120",2);
+insert into summary values (3,"","shenzhang ","20161120",3);
+insert into summary values (4,"","shenzhang ","20161120",4);
+
